017a4ed9f8c9cf310246b4f95f6db5b8e8d6519e
Yash Jain <yashjain07.yj@gmail.com>
2022-12-03 20:01:45 +0530

    Added mapping and enterIntoLottery Function

--- Lottery.sol
+++ Lottery.sol
@@ -6,8 +6,24 @@ contract Lottery{
 address payable public manager;
 address payable[] public Players;
 
+mapping(address => bool) public alreadyEntered;
+
 constructor(){
     manager = msg.sender;
+}
+
+function enterIntoLottery() payable external{
+
+    require(msg.sender != manager,"Manager cannot participate into Lottery");
+
+    require(alreadyEntered[msg.sender] == false,"You can enter only one time");
+
+    require(msg.value == 1 ether,"Amount should be equal to price the Price , Which is 1 ether");
+
+    Players.push(payable(msg.sender));
+
+    alreadyEntered[msg.sender] = true;
+
 }
 
 
